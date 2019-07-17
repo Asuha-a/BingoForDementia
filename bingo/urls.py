6eeb0f366e9d1692b76d55c8b40fdc8ebd8d74ca
Asuha-a <kobayashi.91.1775@docomo.ne.jp>
2019-07-17 12:42:34 +0900

    change category

--- bingo/urls.py
+++ bingo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('category/player', views.category, name='category'),
+    path('category/manager', views.category, name='category'),
     path('', views.top, name='top'),
 ]
--- bingo/views.py
+++ bingo/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def top(request):
-    return render(request, 'bingo/top.html')
+    params = {
+        'mode': 'player',
+    }
+    return render(request, 'bingo/top.html', params)
+
+def category(request):
+    params = {
+        'mode': 'player',
+    }
+    return render(request, 'bingo/category.html', params)
+    lookup_field = 'pk'
