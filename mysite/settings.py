21da833d185827b65428eec5e0bc31f0c4c528f7
Asuha-a <kobayashi.91.1775@docomo.ne.jp>
2019-07-28 20:20:41 +0900

    deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,8 +21,8 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-if not DEBUG:
-    django_heroku.settings(locals())
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
